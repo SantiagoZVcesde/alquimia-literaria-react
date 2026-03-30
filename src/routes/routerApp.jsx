5792765e417f9027aa7344af19e04b070d4b4666
SantiagoZVcesde <zapatavillasantiago@gmail.com>
2026-03-30 18:29:52 -0500

    (feature): Se hacer la enrutacion para la navegacion entre paginas

---
+++ src/routes/routerApp.jsx
@@ -0,0 +1,36 @@
+import Cart from "../pages/Cart";
+import Home from "../pages/Home";
+import Library from "../pages/Library";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import App from "../App";
+
+const routerApp = [
+    {
+        path: "/",
+        element: <App />,
+        
+    },
+    {
+        path: "/home",
+        element: <Home />,
+    },
+    {
+        path: "/library",
+        element: <Library />,
+    },
+    {
+        path: "/cart",
+        element: <Cart />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+];
+
+export default routerApp;
